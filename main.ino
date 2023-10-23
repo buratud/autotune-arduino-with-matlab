8ee78f206158326b6203f1f57b17f8e6cb6f84ec
Buratud Thumboon <buratud@gmail.com>
2023-10-23 11:28:46 +0700

    Make use of buffer

--- main.ino
+++ main.ino
@@ -1,15 +1,13 @@
 #include <core_esp8266_i2s.h>
-#include <pins_arduino.h>
-#include <I2S.h>
 #define BUFFER_SIZE 256
 
 int16_t buffer[BUFFER_SIZE];
 
 void i2sInit()
 {
-  i2s_rxtx_begin(true, false); // This must be on top, I don't know why but moving it below make wave jakky
+  i2s_rxtx_begin(true, false); // This must be on top, I don't know why but moving it below make wave jackky
+  i2s_set_rate(30000);
   i2s_set_bits(24);
-  i2s_set_rate(44100);
 }
 
 void setup()
@@ -22,9 +20,11 @@ void loop()
 {
   for (int i = 0; i < BUFFER_SIZE; i++)
   {
-    if (i2s_read_sample(&buffer[i], nullptr, true))
-    {
-      Serial.println(buffer[i]);
-    }
+    i2s_read_sample(&buffer[i], nullptr, true);
+  }
+  for (int i = 0; i < BUFFER_SIZE; i++)
+  {
+    Serial.write(buffer[i]);
+    // Serial.println(buffer[i]);
   }
 }
