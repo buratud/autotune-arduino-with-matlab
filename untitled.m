559be6b69d9dfc53f617c45561e576a4d3214ebb
reconnecting <tapanawat2004@gmail.com>
2023-11-03 20:56:52 +0700

    add test udp
    not complete

---
+++ untitled.m
@@ -0,0 +1,14 @@
+udpObject = udpport("datagram","IPV4",LocalHost="192.168.1.255",LocalPort=8080);
+start(udpObject);
+disp('UDP Port started.');
+try
+    while true
+        dataReceived = receive(udpObject);
+        % Process the received data here
+        disp(['Received data: ', char(dataReceived)]);
+    end
+catch
+    stop(udpObject);
+    delete(udpObject);
+    disp('UDP Port stopped and deleted.');
+end
