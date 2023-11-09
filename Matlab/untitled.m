6a954fb5f19f0b90760039ef8e22f66b1e21f9f8
reconnecting <tapanawat2004@gmail.com>
2023-11-09 20:05:38 +0700

    add bar
    turn note to frequency not complete

--- Matlab/untitled.m
+++ Matlab/untitled.m
@@ -1,11 +1,16 @@
-
-udpObject = udpport("datagram",LocalHost="192.168.1.124",LocalPort=8080,EnablePortSharing=true)
-try
-    while true
-        udpObject.NumDatagramsAvailable
-        data = read(udpObject,udpObject.NumDatagramsAvailable,"uint32")
-        
-    end
-catch
-    disp('UDP Port stopped and deleted.');
+clear u
+u = udpport("Byte", "IPV4", "LocalPort", 8080, "EnablePortSharing", true);
+filename = 'output.wav';
+fileFormat = 'wav';
+sampleRate = 24000;
+while true
+    x = read(u,240*100,"int32");
+    x = bitshift(x, -8, "int32");
+    x = x / 8388608.0;
+    player = audioplayer(x, sampleRate, 24);
+    play(player);
+    audiowrite(filename, x, sampleRate, 'BitsPerSample', 24);
+    pause(player.TotalSamples/player.SampleRate);
+    % Clean up
+    %stop(player);
 end
